e51ecd5a74ffba2a54dc1fff84e70f24fe71efb7
bogich <880771@mail.ru>
2018-07-24 12:48:58 +0500

    Версия 1.0

---
+++ CheckEGRN/Helpers/HelperWebRequestEGRN.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Configuration;
+
+namespace CheckEGRN.Helpers.HelperWebRequestEGRN
+{
+    /// <summary>
+    /// Универсальные ошибки
+    /// </summary>
+    [DataContract]
+    public class Error
+    {
+        [DataMember(Name = "code")]
+        public string Code { get; set; }
+        [DataMember(Name = "mess")]
+        public string Mess { get; set; }
+    }
+
+    /// <summary>
+    /// Возвращает список найденных объектов по ключевому запросу
+    /// </summary>
+    [DataContract]
+    public class EGRNResponseCadasterSearsh
+    {
+        [DataMember(Name = "objects")]
+        public List<ListObjects> Objects { get; set; }
+        [DataMember(Name = "found")]
+        public int Found { get; set; }
+        [DataMember(Name = "region")]
+        public string Region { get; set; }
+        [DataMember(Name = "error")]
+        public Error Error { get; set; }
+    }
+    [DataContract]
+    public class ListObjects
+    {
+        [DataMember(Name = "CADNOMER")]
+        public string Cadnomer { get; set; }
+        [DataMember(Name = "ADDRESS")]
+        public string Address { get; set; }
+        [DataMember(Name = "TYPE")]
+        public string Type { get; set; }
+        [DataMember(Name = "AREA")]
+        public string Area { get; set; }
+        [DataMember(Name = "CATEGORY")]
+        public string Category { get; set; }
+    }
+
+    /// <summary>
+    /// Возвращает сведения об объекте из ЕГРН, также список доступных документов для заказа
+    /// </summary>
+    [DataContract]
+    public class EGRNResponseCadasterObjectInfoFull
+    {
+        [DataMember(Name = "EGRN")]
+        public EGRNObject EGRN { get; set; }
+        [DataMember(Name = "region")]
+        public string Region { get; set; }
+        [DataMember(Name = "documents")]
+        public DocumentType Documents { get; set; }
+        [DataMember(Name = "encoded_object")]
+        public string Encoded_object { get; set; }
+        [DataMember(Name = "error")]
+        public Error Error { get; set; }
+    }
+    [DataContract]
+    public class EGRNObject
+    {
+        [DataMember(Name = "object")]
+        public ListObjects Object { get; set; }
+        [DataMember(Name = "details")]
+        public ListDetails Details { get; set; }
+    }
+    [DataContract]
+    public class ListDetails
+    {
+        [DataMember(Name = "Тип объекта")]
+        public string ObjectType { get; set; }
+        [DataMember(Name = "Кадастровый номер")]
+        public string CadasterNumber { get; set; }
+        [DataMember(Name = "Статус объекта")]
+        public string ObjectState { get; set; }
+        [DataMember(Name = "Дата постановки на кадастровый учет")]
+        public string DateCadasterState { get; set; }
+        [DataMember(Name = "Категория земель")]
+        public string LandCategory { get; set; }
+        [DataMember(Name = "Разрешенное использование")]
+        public string PermittedUse { get; set; }
+        [DataMember(Name = "Площадь")]
+        public string Square { get; set; }
+        [DataMember(Name = "Единица измерения (код)")]
+        public string MeasureUnit { get; set; }
+        [DataMember(Name = "Кадастровая стоимость")]
+        public string CadasterValue { get; set; }
+        [DataMember(Name = "Дата определения стоимости")]
+        public string DateValue { get; set; }
+        [DataMember(Name = "Дата внесения стоимости")]
+        public string DateDeposit { get; set; }
+        [DataMember(Name = "Дата утверждения стоимости")]
+        public string DateApprovalValue { get; set; }
+        [DataMember(Name = "Адрес (местоположение)")]
+        public string Address { get; set; }
+        [DataMember(Name = "Дата обновления информации")]
+        public string DateInfoUpdate { get; set; }
+        [DataMember(Name = "Форма собственности")]
+        public string OwnerType { get; set; }
+        [DataMember(Name = "Количество правообладателей")]
+        public string RightholdersCount { get; set; }
+        [DataMember(Name = "Кадастровый инженер")]
+        public string CadasterEngineer { get; set; }
+    }
+    [DataContract]
+    public class DocumentType
+    {
+        [DataMember(Name = "XZP")]
+        public DocumentDetails XZP { get; set; }
+        [DataMember(Name = "SOPP")]
+        public DocumentDetails SOPP { get; set; }
+        [DataMember(Name = "KPT")]
+        public DocumentDetails KPT { get; set; }
+    }
+    [DataContract]
+    public class DocumentDetails
+    {
+        [DataMember(Name = "label")]
+        public string Label { get; set; }
+        [DataMember(Name = "available")]
+        public bool Available { get; set; }
+        [DataMember(Name = "price")]
+        public float Price { get; set; }
+    }
+
+    /// <summary>
+    /// Сканирует и возвращает количество помещений входящих в состав многоквартирного дома
+    /// </summary>
+    [DataContract]
+    public class EGRNResponseReestrScan
+    {
+        [DataMember(Name ="scan")]
+        public string Scan { get; set; }
+        [DataMember(Name ="note")]
+        public string Note { get; set; }
+        [DataMember(Name ="reestr")]
+        public Reestr Reestr { get; set; }
+        [DataMember(Name ="error")]
+        public Error Error { get; set; }
+    }
+    [DataContract]
+    public class Reestr
+    {
+        [DataMember(Name ="id")]
+        public int Id { get; set; }
+        [DataMember(Name = "categories")]
+        public ApartmentType Categories { get; set; }
+        [DataMember(Name ="count")]
+        public int Count { get; set; }
+    }
+    [DataContract]
+    public class ApartmentType
+    {
+        [DataMember(Name = "apartments_residential")]
+        public ApartmentCount Apartments_residential { get; set; }
+        [DataMember(Name = "apartments_nonresidential")]
+        public ApartmentCount Apartments_nonresidential { get; set; }
+        [DataMember(Name = "others")]
+        public ApartmentCount Others { get; set; }
+    }
+    [DataContract]
+    public class ApartmentCount
+    {
+        [DataMember(Name = "count")]
+        public int Count { get; set; }
+    }
+
+    class HelperWebRequestEGRN
+    {
+        /// <summary>
+        /// Возвращает список найденных объектов по ключевому запросу
+        /// </summary>
+        /// <param name="_query">Kадастровый номер или адрес</param>
+        /// <returns></returns>
+        public EGRNResponseCadasterSearsh WRCadasterSearch(string _query)
+        {
+            string _parQuery = @"query=" + _query;
+            byte[] _arrQuery = Encoding.UTF8.GetBytes(_parQuery);
+
+            WebRequest _webRequest = (HttpWebRequest)WebRequest.Create("https://apirosreestr.ru/api/cadaster/search");
+            _webRequest.ContentType = "application/x-www-form-urlencoded";
+            _webRequest.Method = "POST";
+            _webRequest.Headers.Add("Token", ConfigurationManager.AppSettings["_token"]);
+            _webRequest.ContentLength = _arrQuery.Length;
+
+            Stream dataStream = _webRequest.GetRequestStream();
+            dataStream.Write(_arrQuery, 0, _arrQuery.Length);
+            dataStream.Close();
+
+            WebResponse _webResponse = _webRequest.GetResponse();
+            Stream streamResponse = _webResponse.GetResponseStream();
+            StreamReader streamReader = new StreamReader(streamResponse);
+            string Response = streamReader.ReadToEnd();
+
+            DataContractJsonSerializer _dataContractJsonSerializer = new DataContractJsonSerializer(typeof(EGRNResponseCadasterSearsh));
+            EGRNResponseCadasterSearsh _result = (EGRNResponseCadasterSearsh)_dataContractJsonSerializer.ReadObject(new MemoryStream(Encoding.Unicode.GetBytes(Response)));
+
+            return _result;
+        }
+
+        /// <summary>
+        /// Возвращает сведения об объекте из ЕГРН, также список доступных документов для заказа
+        /// </summary>
+        /// <param name="_query">Только кадастровый номер</param>
+        /// <returns></returns>
+        public EGRNResponseCadasterObjectInfoFull WRCadasterObjectInfoFull(string _query)
+        {
+            string _parQuery = @"query=" + _query;
+            byte[] _arrQuery = Encoding.UTF8.GetBytes(_parQuery);
+
+            WebRequest _webRequest = (HttpWebRequest)WebRequest.Create("https://apirosreestr.ru/api/cadaster/objectInfoFull");
+            _webRequest.ContentType = "application/x-www-form-urlencoded";
+            _webRequest.Method = "POST";
+            _webRequest.Headers.Add("Token", ConfigurationManager.AppSettings["_token"]);
+            _webRequest.ContentLength = _arrQuery.Length;
+
+            Stream dataStream = _webRequest.GetRequestStream();
+            dataStream.Write(_arrQuery, 0, _arrQuery.Length);
+            dataStream.Close();
+
+            WebResponse _webResponse = _webRequest.GetResponse();
+            Stream streamResponse = _webResponse.GetResponseStream();
+            StreamReader streamReader = new StreamReader(streamResponse);
+            string Response = streamReader.ReadToEnd();
+
+            DataContractJsonSerializer _dataContractJsonSerializer = new DataContractJsonSerializer(typeof(EGRNResponseCadasterObjectInfoFull));
+            EGRNResponseCadasterObjectInfoFull _result = (EGRNResponseCadasterObjectInfoFull)_dataContractJsonSerializer.ReadObject(new MemoryStream(Encoding.Unicode.GetBytes(Response)));
+
+            return _result;
+        }
+
+        /// <summary>
+        /// Сканирует и возвращает количество помещений входящих в состав многоквартирного дома
+        /// </summary>
+        /// <param name="_query">Кадастровый номер МКД</param>
+        /// <returns></returns>
+        public EGRNResponseReestrScan WRReestrScan(string _query)
+        {
+            string _parQuery = @"query=" + _query; // + "&scan_if_no_cache=1";
+            byte[] _arrQuery = Encoding.UTF8.GetBytes(_parQuery);
+
+            WebRequest _webRequest = (HttpWebRequest)WebRequest.Create("https://apirosreestr.ru/api/reestr/scan");
+            _webRequest.ContentType = "application/x-www-form-urlencoded";
+            _webRequest.Method = "POST";
+            _webRequest.Headers.Add("Token", ConfigurationManager.AppSettings["_token"]);
+            _webRequest.ContentLength = _arrQuery.Length;
+
+            Stream dataStream = _webRequest.GetRequestStream();
+            dataStream.Write(_arrQuery, 0, _arrQuery.Length);
+            dataStream.Close();
+
+            WebResponse _webResponse = _webRequest.GetResponse();
+            Stream streamResponse = _webResponse.GetResponseStream();
+            StreamReader streamReader = new StreamReader(streamResponse);
+            string Response = streamReader.ReadToEnd();
+
+            DataContractJsonSerializer _dataContractJsonSerializer = new DataContractJsonSerializer(typeof(EGRNResponseReestrScan));
+            EGRNResponseReestrScan _result = (EGRNResponseReestrScan)_dataContractJsonSerializer.ReadObject(new MemoryStream(Encoding.Unicode.GetBytes(Response)));
+
+            return _result;
+        }
+    }
+}
---
+++ CheckEGRN/Program.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading;
+using CheckEGRN.Helpers.HelperWebRequestEGRN;
+using System.Data.SqlClient;
+
+namespace CheckEGRN
+{
+    class Program
+    {
+        static string _address;
+        static void Main(string[] args)
+        {
+            //string _address = "Екатеринбург, Уральская 46";
+            //string _cadnomer = "";
+
+            List<string> _listCityAddress = new List<string>();
+            string connectionString = @"Data Source=storage;Initial Catalog=FiasSQL;User Id=sa;Password=SQLsa2012";
+            string sqlExpression = @"SELECT
+                                        CONCAT
+                                        (REPLACE(lev1.SHORTNAME, ' ', '')
+                                        ,' '
+                                        ,REPLACE(lev1.OFFNAME, ' ', '')
+                                        ,' '
+                                        ,REPLACE(lev2.SHORTNAME, ' ', '')
+                                        ,' '
+                                        ,REPLACE(lev2.OFFNAME, ' ', '')
+                                        ,' '
+                                        ,REPLACE(lev4.HOUSENUM, ' ', '')
+                                        ,' '
+                                        ,REPLACE(lev4.BUILDNUM, ' ', '')
+                                        ,' '
+                                        ,REPLACE(lev4.STRUCNUM, ' ', '')) AS CityAddress
+                                    FROM
+                                        FiasSQL.dbo.addrObj lev0
+                                        LEFT JOIN addrObj lev1 ON lev0.AOGUID = lev1.PARENTGUID
+                                        LEFT JOIN addrObj lev2 ON lev1.AOGUID = lev2.PARENTGUID
+                                        LEFT JOIN house lev4 ON lev2.AOGUID = lev4.AOGUID
+                                    WHERE
+                                        lev0.STATUS = 1
+                                        AND lev1.STATUS = 1
+                                        AND lev2.STATUS = 1
+                                        AND lev1.OFFNAME = 'Екатеринбург'
+                                        AND lev4.ENDDATE = '2079-06-06'";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    if (reader.HasRows)
+                    {
+                        _address = reader["CityAddress"].ToString();
+                        _listCityAddress.Add(_address);
+                    }
+                }
+                connection.Close();
+            }
+
+            HelperWebRequestEGRN helperWebRequestEGRN = new HelperWebRequestEGRN();
+
+            List<string[]> _listCadasterNumber = new List<string[]>();
+            foreach(var _itemCityAddress in _listCityAddress)
+            {
+                EGRNResponseCadasterSearsh cadasterSearsh = helperWebRequestEGRN.WRCadasterSearch(_itemCityAddress);
+                if (cadasterSearsh.Error.Code == null)
+                {
+                    Console.WriteLine("Found: {0}", cadasterSearsh.Found);
+                    Console.WriteLine("Region: {0}", cadasterSearsh.Region);
+                    foreach (var itemCadasterSearch in cadasterSearsh.Objects)
+                    {
+                        Console.WriteLine("----------");
+                        Console.WriteLine("=> Address: {0}", itemCadasterSearch.Address);
+                        Console.WriteLine("=> Area: {0}", itemCadasterSearch.Area);
+                        Console.WriteLine("=> Cadnomer: {0}", itemCadasterSearch.Cadnomer);
+                        Console.WriteLine("=> Category: {0}", itemCadasterSearch.Category);
+                        Console.WriteLine("=> Type: {0}", itemCadasterSearch.Type);
+
+                        if (itemCadasterSearch.Type == "Здание")
+                        {
+                            _listCadasterNumber.Add(new string[] { _itemCityAddress, itemCadasterSearch.Cadnomer });
+                        }
+                    }
+                    Console.WriteLine("====================");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Error: {0} - {1}", cadasterSearsh.Error.Code, cadasterSearsh.Error.Mess);
+                    Console.ResetColor();
+                }
+
+                //EGRNResponseCadasterObjectInfoFull cadasterObjectInfoFull = helperWebRequestEGRN.WRCadasterObjectInfoFull(_cadnomer);
+                //if (cadasterObjectInfoFull.Error.Code == null)
+                //{
+                //    Console.WriteLine("Region: {0}", cadasterObjectInfoFull.Region);
+                //    Console.WriteLine("XZP Available: {0}",cadasterObjectInfoFull.Documents.XZP.Available);
+                //    Console.WriteLine("XZP Label: {0}",cadasterObjectInfoFull.Documents.XZP.Label);
+                //    Console.WriteLine("XZP Price: {0}",cadasterObjectInfoFull.Documents.XZP.Price);
+                //    Console.WriteLine("SOPP Available: {0}", cadasterObjectInfoFull.Documents.SOPP.Available);
+                //    Console.WriteLine("SOPP Label: {0}", cadasterObjectInfoFull.Documents.SOPP.Label);
+                //    Console.WriteLine("SOPP Price: {0}", cadasterObjectInfoFull.Documents.SOPP.Price);
+                //    Console.WriteLine("KPT Available: {0}", cadasterObjectInfoFull.Documents.KPT.Available);
+                //    Console.WriteLine("KPT Label: {0}:", cadasterObjectInfoFull.Documents.KPT.Label);
+                //    Console.WriteLine("KPT Price: {0}:", cadasterObjectInfoFull.Documents.KPT.Price);
+                //    Console.WriteLine("----------");
+                //    Console.WriteLine("Address: {0}", cadasterObjectInfoFull.EGRN.Object.Address);
+                //    Console.WriteLine("Area: {0}", cadasterObjectInfoFull.EGRN.Object.Area);
+                //    Console.WriteLine("Cadnomer: {0}", cadasterObjectInfoFull.EGRN.Object.Cadnomer);
+                //    Console.WriteLine("Category: {0}", cadasterObjectInfoFull.EGRN.Object.Category);
+                //    Console.WriteLine("Type: {0}", cadasterObjectInfoFull.EGRN.Object.Type);
+                //    Console.WriteLine("----------");
+                //    Console.WriteLine("Address: {0}", cadasterObjectInfoFull.EGRN.Details.Address);
+                //    Console.WriteLine("Cadaster engineer: {0}", cadasterObjectInfoFull.EGRN.Details.CadasterEngineer);
+                //    Console.WriteLine("Cadaster number: {0}", cadasterObjectInfoFull.EGRN.Details.CadasterNumber);
+                //    Console.WriteLine("Cadaster value: {0}", cadasterObjectInfoFull.EGRN.Details.CadasterValue);
+                //    Console.WriteLine("Date approval value: {0}", cadasterObjectInfoFull.EGRN.Details.DateApprovalValue);
+                //    Console.WriteLine("Date cadaster state: {0}", cadasterObjectInfoFull.EGRN.Details.DateCadasterState);
+                //    Console.WriteLine("Date deposit: {0}", cadasterObjectInfoFull.EGRN.Details.DateDeposit);
+                //    Console.WriteLine("Date info update: {0}", cadasterObjectInfoFull.EGRN.Details.DateInfoUpdate);
+                //    Console.WriteLine("Date value: {0}", cadasterObjectInfoFull.EGRN.Details.DateValue);
+                //    Console.WriteLine("Land category: {0}", cadasterObjectInfoFull.EGRN.Details.LandCategory);
+                //    Console.WriteLine("Measure unit: {0}", cadasterObjectInfoFull.EGRN.Details.MeasureUnit);
+                //    Console.WriteLine("Object state: {0}", cadasterObjectInfoFull.EGRN.Details.ObjectState);
+                //    Console.WriteLine("Object type: {0}", cadasterObjectInfoFull.EGRN.Details.ObjectType);
+                //    Console.WriteLine("Owner type: {0}", cadasterObjectInfoFull.EGRN.Details.OwnerType);
+                //    Console.WriteLine("Permitted use: {0}", cadasterObjectInfoFull.EGRN.Details.PermittedUse);
+                //    Console.WriteLine("Rightholders count: {0}", cadasterObjectInfoFull.EGRN.Details.RightholdersCount);
+                //    Console.WriteLine("Square: {0}", cadasterObjectInfoFull.EGRN.Details.Square);
+                //
+                //    Console.WriteLine("====================");
+                //}
+                //else
+                //{
+                //    Console.ForegroundColor = ConsoleColor.Red;
+                //    Console.WriteLine("Error: {0} - {1}", cadasterObjectInfoFull.Error.Code, cadasterObjectInfoFull.Error.Mess);
+                //    Console.ResetColor();
+                //}
+            }
+
+            foreach (var _itemCadasterNumber in _listCadasterNumber)
+            {
+                int i = 0;
+                EGRNResponseReestrScan reestrScan = helperWebRequestEGRN.WRReestrScan(_itemCadasterNumber[1]);
+                if (reestrScan.Error.Code == null)
+                {
+                    Console.WriteLine("Note: {0}", reestrScan.Note);
+                    //do
+                    //{
+                    //    Console.Write("{0} sec left", i);
+                    //    Console.SetCursorPosition(0, Console.CursorTop);
+                    //    Thread.Sleep(1000);
+                    //    i++;
+                    //}
+                    //while (reestrScan.Scan == "processing");
+
+                    //Console.WriteLine("Scan: {0}", reestrScan.Scan);
+                    //Console.WriteLine("Note: {0}", reestrScan.Note);
+                    //Console.WriteLine("Id: {0}", reestrScan.Reestr.Id);
+                    //Console.WriteLine("Count: {0}", reestrScan.Reestr.Count);
+                    //Console.WriteLine("Apartments_residential: {0}", reestrScan.Reestr.Categories.Apartments_residential.Count);
+                    //Console.WriteLine("Apartments_nonresidential: {0}", reestrScan.Reestr.Categories.Apartments_nonresidential.Count);
+                    //Console.WriteLine("Others: {0}", reestrScan.Reestr.Categories.Others.Count);
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Error: {0} - {1}", reestrScan.Error.Code, reestrScan.Error.Mess);
+                    Console.ResetColor();
+                }
+
+                File.AppendAllText(@"LogResult.csv", DateTime.Now + _itemCadasterNumber[0] + ";" + _itemCadasterNumber[1] + ";" + reestrScan.Reestr.Categories.Apartments_residential.Count + ";" + reestrScan.Reestr.Categories.Apartments_nonresidential.Count + ";" + reestrScan.Reestr.Categories.Others.Count + ";" + Environment.NewLine, Encoding.Default);
+            }
+
+            Console.WriteLine("Scan completed success");
+            Console.Read();
+        }
+    }
+}
